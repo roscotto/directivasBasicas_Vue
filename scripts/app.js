044e37b98343e3965fd04d2d792d601196e03d71
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-04-03 14:30:03 -0300

    upload files

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+var app= new Vue({
+	el: ".contenedor",
+	data: {
+		title: "Super Mario 3d World + Bowser´s Fury",
+		imagen_mario: "img/mario.jpg",
+		alt: "portada del juego de Super Mario",
+		h2_mario: "<h2>Salvá el Reino de las hadas!</h2>",
+		h2_bowser: "Explora un mundo felino con Bowser’s Fury",
+		datazo: false,
+		}
+})
+
